7d34681733b5b4fe99b75236cef53959c9a6703d
Wiby Putra Adyan Ramdhani <wibbyramdhani@gmail.com>
2019-07-10 00:32:22 +0700

    Login & Register

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
+
 const homeRouter = require('./routes/home');
+const userRouter = require('./routes/user');
 
 app.use("/", homeRouter);
+app.use('/user', userRouter);
 
 app.listen(3000, () => {
     console.log('Server nyala bro');
