140eadc718039092100b8bbc1f9fbe67155fa7d2
Wiby Putra Adyan Ramdhani <wibbyramdhani@gmail.com>
2019-07-10 00:58:58 +0700

    update

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const UserController = require('../controllers/user');
 
 const router = express.Router();
 
-// login & Register
+// ini login & Register
 router.post('/register', UserController.postRegister);
 router.post('/login', UserController.postLogin);
 
