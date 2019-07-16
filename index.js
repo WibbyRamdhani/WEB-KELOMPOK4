5401af5140b6ca019f3618c412b9032630e9e25b
Wiby Putra Adyan Ramdhani <wibbyramdhani@gmail.com>
2019-07-17 00:43:18 +0700

    order sudah beres

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ app.use(bodyParser.json());
 const homeRouter = require('./routes/home');
 const userRouter = require('./routes/user');
 const booksRouter = require('./routes/books');
+const orderRouter = require('./routes/order');
 
 const sequelize = require('./configs/sequelize');
 
 app.use("/", homeRouter);
 app.use('/user', userRouter);
 app.use('/books', booksRouter);
+app.use('/order', orderRouter);
 
 app.listen(3000, () => {
     console.log('Server nyala bro');
